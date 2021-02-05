823d93b0dd630a2e77ae5322325b3249fcdbd084
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2021-02-06 02:56:44 +0530

    outline completed

---
+++ src/components/services.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import sketch from "./sketch.svg";
+
+export class Services extends Component {
+  render() {
+    return (
+      <div id="services" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>Our Services</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+              dapibus leonec.
+            </p>
+          </div>
+          <div className="row">
+            {this.props.data
+              ? this.props.data.map((d, i) => (
+                  <div key={`${d.name}-${i}`} className="col-md-4">
+                    {" "}
+                    <img alt="dadad" src={sketch} height="100" width="auto" />
+                    <div className="service-desc">
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
+                  </div>
+                ))
+              : "loading"}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Services;
